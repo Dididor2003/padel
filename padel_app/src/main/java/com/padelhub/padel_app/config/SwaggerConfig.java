914b5a20a66f382b12dafb0af8cd00fdb16d3ba3
adamone <adamespilla@gmail.com>
2026-02-17 19:54:12 +0100

    SwaggerConfig afegit

---
+++ padel_app/src/main/java/com/padelhub/padel_app/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.padelhub.padel_app.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("PadelHub API REST")
+                        .version("1.0")
+                        .description("API per a la gestió de socis i reserves del club de padel"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
